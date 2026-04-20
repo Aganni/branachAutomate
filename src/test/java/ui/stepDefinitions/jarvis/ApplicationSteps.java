850c670f522e6a1a8f98570092b025a24a93cb61
Aganni <157684232+Aganni@users.noreply.github.com>
2026-04-20 21:04:58 +0530

    we

--- src/test/java/ui/stepDefinitions/jarvis/ApplicationSteps.java
+++ src/test/java/ui/stepDefinitions/jarvis/ApplicationSteps.java
@@ -12,7 +12,7 @@ public class ApplicationSteps {
     @And("User navigates to the Applications tab searches appFrom using {string} and opens the appform")
     public void openAppFromInJarvis(String searchType) {
         jarvisAppPage.navigateToApplicationTab();
-        jarvisAppPage.searchByCriteria(searchType, "dsa-3d3e3f75-38d3-4dcf-b2e5-3c3c1dc279ad");
+        jarvisAppPage.searchByCriteria(searchType, "dsa-1c9e9659-0371-4a9f-ac13-f5013d4556da");
         jarvisAppPage.openFirstApplication();
     }
 }
