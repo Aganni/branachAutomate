baf8dd085946db2898240dafdc148653988f82b4
Aganni <157684232+Aganni@users.noreply.github.com>
2026-05-06 13:42:39 +0530

    FIxed loan requirment fix
    FIxed loan requirment fix

--- src/test/java/ui/stepDefinitions/jarvis/ApplicationSteps.java
+++ src/test/java/ui/stepDefinitions/jarvis/ApplicationSteps.java
@@ -18,7 +18,7 @@ public class ApplicationSteps extends BaseTest {
     public void openAppFromInJarvis(String searchType) throws InterruptedException {
         dashboardPage.navigateToApplicationTab();
 
-        applicationDashboardPage.searchByCriteria(searchType, "dsa-13accc2d-a1f2-4fef-9cc5-15361d009ead");
+        applicationDashboardPage.searchByCriteria(searchType, "dsa-ae6cc320-3088-4dd0-80cb-65525064a051");
 
         // Capture and store App ID before clicking open — needed for Allocation
         DynamicDataClass.setValue("appFormId", applicationDashboardPage.getAppFormIdFromFirstRow());
